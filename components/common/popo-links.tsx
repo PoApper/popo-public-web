84ca8a73c313848a968d88794e71889814c4180f
Gwanho Kim <khkim6040@gmail.com>
2025-03-09 21:06:55 +0900

    Fix/delivery address (#121)

---
+++ components/common/popo-links.tsx
@@ -0,0 +1,8 @@
+const StudentCouncilArchiveLink =
+  'https://drive.google.com/drive/u/0/folders/1vHexwLSdD92maoKNlvw9zQ0q0J59k5FD';
+const PostechDeliveryLink = 'https://www.postechdorm.com/delivery';
+
+export const POPOLinks = {
+  StudentCouncilArchiveLink,
+  PostechDeliveryLink,
+};
