931137ce496b4327f86322205d4a13739bfcd5c2
Gwanho Kim <khkim6040@gmail.com>
2025-09-13 22:31:16 +0900

    Chore: Make explicit fail when env does not exist

--- lib/axios.instance.ts
+++ lib/axios.instance.ts
@@ -9,6 +9,13 @@ import {
 
 const next_env = process.env.NEXT_PUBLIC_ENV;
 
+if (!next_env) {
+  throw new Error(
+    '.env 파일에 NEXT_PUBLIC_ENV 환경변수가 설정되어 있지 않습니다.',
+  );
+}
+
+
 export const popoApiUrl =
   next_env === 'prod'
     ? 'https://api.popo.poapper.club'
@@ -16,7 +23,11 @@ export const popoApiUrl =
       ? 'https://api.popo-dev.poapper.club'
       : next_env === 'local'
         ? 'https://localhost:4000'
-        : new Error('NEXT_PUBLIC_ENV is not set or invalid');
+        : (() => {
+          throw new Error(
+            `NEXT_PUBLIC_ENV 환경변수가 제대로 설정되어 있지 않습니다. ${next_env}는 유효하지 않은 값입니다.`,
+          );
+        })();
 
 export const PoPoAxios = axios.create({
   baseURL: popoApiUrl as string,
