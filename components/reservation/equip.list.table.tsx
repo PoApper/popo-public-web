03c82ba720f309710195cf1f818800bcb6d1f6e6
hegelty <dev@hegelty.me>
2025-11-26 11:24:00 +0900

    feat: show opening time info in list

--- components/reservation/equip.list.table.tsx
+++ components/reservation/equip.list.table.tsx
@@ -1,6 +1,7 @@
 import { Image, Modal, Table } from 'semantic-ui-react';
 
 import { IEquipment } from '@/types/reservation.interface';
+import OpeningHoursList from '@/components/reservation/opening_hours.list';
 
 const EquipListTable = ({ equipments }: { equipments: IEquipment[] }) => {
   return (
@@ -16,7 +17,7 @@ const EquipListTable = ({ equipments }: { equipments: IEquipment[] }) => {
         {equipments.map((equipment: any, idx: number) => (
           <Modal
             key={idx}
-            size={'mini'}
+            size={'small'}
             trigger={
               <Table.Row>
                 <Table.Cell>{idx + 1}</Table.Cell>
@@ -25,6 +26,7 @@ const EquipListTable = ({ equipments }: { equipments: IEquipment[] }) => {
               </Table.Row>
             }
           >
+            <Modal.Header>{equipment.name}</Modal.Header>
             <Modal.Content>
               <Image
                 src={
@@ -33,9 +35,28 @@ const EquipListTable = ({ equipments }: { equipments: IEquipment[] }) => {
                 }
                 alt={`${equipment.name}_logo`}
               />
-              <pre style={{ whiteSpace: 'pre-wrap' }}>
-                {equipment.description}
-              </pre>
+              <div style={{ marginTop: '1rem' }}>
+                <h4>설명</h4>
+                <pre style={{ whiteSpace: 'pre-wrap' }}>
+                  {equipment.description}
+                </pre>
+              </div>
+              <div style={{ marginTop: '1rem' }}>
+                <h4>사용 가능 시간</h4>
+                <OpeningHoursList
+                  openingHours={JSON.parse(equipment.openingHours)}
+                />
+              </div>
+              <div style={{ marginTop: '1rem' }}>
+                <h4>예약비</h4>
+                <p>{equipment.fee.toLocaleString()}원</p>
+              </div>
+              {equipment.maxMinutes && (
+                <div style={{ marginTop: '1rem' }}>
+                  <h4>최대 예약 가능 시간</h4>
+                  <p>{equipment.maxMinutes}분</p>
+                </div>
+              )}
             </Modal.Content>
           </Modal>
         ))}
