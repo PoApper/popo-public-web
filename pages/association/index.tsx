30a3be88cf3ea7d6991f57e69d7c9992a7bcf48b
joon363 <joon363@naver.com>
2025-12-29 23:24:28 +0900

    feat: added association category indicator, sort by category

--- pages/association/index.tsx
+++ pages/association/index.tsx
@@ -5,30 +5,72 @@ import { Image } from 'semantic-ui-react';
 
 import Layout from '@/components/layout';
 import { PoPoAxios } from '@/lib/axios.instance';
-import { IAssociationIntroduce } from '@/types/introduce.interface';
+import {
+  IAssociationIntroduce,
+  IAssociationCategory,
+} from '@/types/introduce.interface';
+
+interface IGroupedAssociation {
+  category: IAssociationCategory;
+  associations: IAssociationIntroduce[];
+}
 
 const AssociationIndexPage: React.FunctionComponent<{
   associationList: IAssociationIntroduce[];
 }> = ({ associationList }) => {
+  // 카테고리 ID를 기준으로 그룹화
+  const groupedData = associationList.reduce(
+    (acc, item) => {
+      const catId = item.categoryId;
+      if (!acc[catId]) {
+        acc[catId] = {
+          category: item.category,
+          associations: [],
+        };
+      }
+      acc[catId].associations.push(item);
+      return acc;
+    },
+    {} as Record<number, IGroupedAssociation>,
+  );
+
+  // 카테고리 ID 순서대로 정렬하여 배열로 변환
+  const sortedCategories = Object.values(groupedData).sort(
+    (a, b) => a.category.id - b.category.id,
+  );
+
   return (
     <Layout>
-      <IntroduceGrid>
-        {associationList.map((intro) => (
-          <div key={intro.uuid}>
-            <Image
-              centered
-              size="small"
-              href={`/association/introduce/${intro.name}`}
-              src={
-                intro.imageUrl ??
-                'https://react.semantic-ui.com/images/wireframe/image.png'
-              }
-              alt={`${intro.name}_logo`}
-            />
-            <AssociationName>{intro.name}</AssociationName>
-          </div>
+      <PageContainer>
+        {sortedCategories.map((group) => (
+          <CategorySection key={group.category.id}>
+            <CategoryHeader>{group.category.displayName}</CategoryHeader>
+            <IntroduceGrid>
+              {group.associations.map((intro) => (
+                <div key={intro.uuid}>
+                  <Image
+                    centered
+                    size="small"
+                    href={`/association/introduce/${intro.name}`}
+                    src={
+                      intro.imageUrl ??
+                      'https://react.semantic-ui.com/images/wireframe/image.png'
+                    }
+                    alt={`${intro.name}_logo`}
+                    style={{
+                      width: '150px',
+                      height: '150px',
+                      objectFit: 'cover',
+                      objectPosition: 'center',
+                    }}
+                  />
+                  <AssociationName>{intro.name}</AssociationName>
+                </div>
+              ))}
+            </IntroduceGrid>
+          </CategorySection>
         ))}
-      </IntroduceGrid>
+      </PageContainer>
     </Layout>
   );
 };
@@ -36,15 +78,42 @@ const AssociationIndexPage: React.FunctionComponent<{
 export default AssociationIndexPage;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await PoPoAxios.get<IAssociationIntroduce[]>(
-    'introduce/association',
-  );
-  const associationList = res.data;
+  try {
+    const res = await PoPoAxios.get<IAssociationIntroduce[]>(
+      'introduce/association',
+    );
+    const associationList = res.data;
 
-  return {
-    props: { associationList },
-  };
+    return {
+      props: { associationList },
+    };
+  } catch (e) {
+    return {
+      props: { associationList: [] },
+    };
+  }
 };
+
+const PageContainer = styled.div`
+  padding: 2rem 0;
+`;
+
+const CategorySection = styled.section`
+  margin-bottom: 4rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const CategoryHeader = styled.h2`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 2rem;
+  padding-bottom: 0.5rem;
+  border-bottom: 2px solid #eee;
+  color: #333;
+`;
 
 const IntroduceGrid = styled.div`
   display: grid;
